7057d8107b711ec4542c24a5b5ecbdb78359ad95
Wendel <wendel.isaac12@gmail.com>
2024-02-05 15:54:12 -0300

    feat: adicionando funções

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const pool = new Pool({
 });
 
 // Middleware
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -29,10 +30,78 @@ app.get('/alunos', async (req, res) => {
   }
 });
 
+app.post('/alunos/pesquisar-alunos', async (req, res) => {
+  const { matricula, turno, serie } = req.body;
+
+  try {
+    let query = 'SELECT * FROM alunos WHERE 1=1';
+    const params = [];
+
+    if (matricula) {
+      query += ' AND matricula = $' + (params.push(matricula));
+    }
+
+    if (turno) {
+      query += ' AND horario = $' + (params.push(turno));
+    }
+
+    if (serie) {
+      query += ' AND serie = $' + (params.push(serie));
+    }
+
+    if (params.length === 0) {
+      query = 'SELECT * FROM alunos';
+    }
+
+    const result = await pool.query(query, params);
+
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Error searching students', error);
+    res.status(500).json({ error: 'Internal Server Error', message: error.message });
+  }
+});
+
+app.get('/alunos/:id', async (req, res) => {
+  const studentId = req.params.id;
+
+  try {
+    const result = await pool.query('SELECT * FROM alunos WHERE id = $1', [studentId]);
+
+    // Check if the student was found
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: 'Student not found' });
+    }
+
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error('Error fetching student', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
 app.post('/alunos', async (req, res) => {
   const newStudent = req.body;
   try {
-    const result = await pool.query( 'INSERT INTO alunos (nome_aluno, nome_responsavel, data_nascimento, serie, data_pagamento, cpf_responsavel, telefone, endereco, cep, identidade_aluno, horario, valor_mensalidade) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *', [ newStudent.nomeAluno, newStudent.nomeResponsavel, newStudent.dataNascimento, newStudent.serie, newStudent.dataPagamento, newStudent.cpfResponsavel, newStudent.telefone, newStudent.endereco, newStudent.cep, newStudent.identidadeAluno, newStudent.horario, newStudent.valorMensalidade, ] );
+    const result = await pool.query(
+      'INSERT INTO alunos (matricula, nome_aluno, nome_responsavel, data_nascimento, serie, data_pagamento, cpf_responsavel, telefone, endereco, cep, identidade_aluno, horario, valor_mensalidade) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *',
+      [
+        newStudent.matricula,
+        newStudent.nome_aluno,
+        newStudent.nome_responsavel,
+        newStudent.data_nascimento,
+        newStudent.serie,
+        newStudent.data_pagamento,
+        newStudent.cpf_responsavel,
+        newStudent.telefone,
+        newStudent.endereco,
+        newStudent.cep,
+        newStudent.identidade_aluno,
+        newStudent.horario,
+        newStudent.valor_mensalidade,
+      ]
+    );
     res.json(result.rows[0]);
   } catch (error) {
     console.error('Error adding student', error);
@@ -60,6 +129,26 @@ app.delete('/alunos/:id', async (req, res) => {
       res.status(500).json({ error: 'Internal Server Error' });
     }
   });
+
+  app.put('/alunos/atualizar-pagamento/:id', async (req, res) => {
+    const { id } = req.params;
+    const { data_pagamento } = req.body;
+    try {
+      const result = await pool.query(
+        'UPDATE alunos SET data_pagamento = $1 WHERE id = $2 RETURNING *',
+        [data_pagamento, id]
+      );
+      if (result.rows.length > 0) {
+        res.json(result.rows[0]);
+      } else {
+        res.status(404).json({ error: 'Student not found' });
+      }
+    } catch (error) {
+      console.error('Error updating payment date', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+  
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
